da53145bf639673fd494e2c5c79463d2210fcf4f
ThFenixXD <89816163+ThFenixXD@users.noreply.github.com>
2023-11-17 08:53:11 -0300

    Nova Webform

---
+++ GCA (Gerenciador de Chaves de Ativação)/WebForms/WebForm1.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GCA__Gerenciador_de_Chaves_de_Ativação_.WebForms
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
